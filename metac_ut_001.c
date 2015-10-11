a03f366d21113d8fd84b590ef7a00a1eeae25cdf
Alexey Odinokov <alexey.odinokov.82@gmail.com>
2015-10-11 22:12:51 +0400

    preparation for unittests

--- metac_ut_001.c
+++ metac_ut_001.c
@@ -14,7 +14,7 @@
  * 1. objects of exported metatype
  * 1.1. define type (many different cases:
  *  base types (https://en.wikipedia.org/wiki/C_data_types), pointers,
- *  typedefs, structures, unions, arrays, enums, their combinations)
+ *  typedefs, enums, arrays, structures, unions, their combinations)
  *  bonus:va_list, arrays with undefined length?
  * 1.2. export metatype based on that type
  * 1.3. create object by using the metatype definition
@@ -33,6 +33,135 @@
  *
  * 7. serialization/deserialization - TBD: status from json - not in this ut
  */
+
+
+/**
+ * Know issues and workarounds:
+ * 1. signed/unsinged <char|short|int|long>, long long, void*(all pointers and arrays) won't work
+ * because they are not one-work types.
+ * use typedefs to export that types via METAC
+ */
+
+/* export base types */
+METAC_EXPORT_TYPE(char);
+METAC_EXPORT_TYPE(short);
+METAC_EXPORT_TYPE(int);
+METAC_EXPORT_TYPE(long);
+METAC_EXPORT_TYPE(float);
+METAC_EXPORT_TYPE(double);
+
+/* create typedefs for non-one-word base types and export them */
+/*char*/
+typedef char char_t;
+typedef signed char schar_t;
+typedef unsigned char uchar_t;
+
+METAC_EXPORT_TYPE(char_t);
+METAC_EXPORT_TYPE(schar_t);
+METAC_EXPORT_TYPE(uchar_t);
+
+/*short*/
+typedef short short_t;
+typedef short int shortint_t;
+typedef signed short sshort_t;
+typedef signed short int sshortint_t;
+typedef unsigned short ushort_t;
+typedef unsigned short int ushortint_t;
+
+METAC_EXPORT_TYPE(short_t);
+METAC_EXPORT_TYPE(shortint_t);
+METAC_EXPORT_TYPE(sshort_t);
+METAC_EXPORT_TYPE(sshortint_t);
+METAC_EXPORT_TYPE(ushort_t);
+METAC_EXPORT_TYPE(ushortint_t);
+
+/*int*/
+typedef int int_t;
+typedef signed signed_t;
+typedef signed int sint_t;
+typedef unsigned unsigned_t;
+typedef unsigned int uint_t;
+
+METAC_EXPORT_TYPE(int_t);
+METAC_EXPORT_TYPE(signed_t);
+METAC_EXPORT_TYPE(sint_t);
+METAC_EXPORT_TYPE(unsigned_t);
+METAC_EXPORT_TYPE(uint_t);
+
+/*long*/
+typedef long long_t;
+typedef long int longint_t;
+typedef signed long slong_t;
+typedef signed long int slongint_t;
+typedef unsigned long ulong_t;
+typedef unsigned long int ulongint_t;
+
+METAC_EXPORT_TYPE(long_t);
+METAC_EXPORT_TYPE(longint_t);
+METAC_EXPORT_TYPE(slong_t);
+METAC_EXPORT_TYPE(slongint_t);
+METAC_EXPORT_TYPE(ulong_t);
+METAC_EXPORT_TYPE(ulongint_t);
+
+/*long long*/
+typedef long long llong_t;
+typedef long long int llongint_t;
+typedef signed long long sllong_t;
+typedef signed long long int sllongint_t;
+typedef unsigned long long ullong_t;
+typedef unsigned long long int ullongint_t;
+
+METAC_EXPORT_TYPE(llong_t);
+METAC_EXPORT_TYPE(llongint_t);
+METAC_EXPORT_TYPE(sllong_t);
+METAC_EXPORT_TYPE(sllongint_t);
+METAC_EXPORT_TYPE(ullong_t);
+METAC_EXPORT_TYPE(ullongint_t);
+
+/*float*/
+typedef float float_t;
+METAC_EXPORT_TYPE(float_t);
+
+/*double*/
+typedef double double_t;
+METAC_EXPORT_TYPE(double_t);
+
+/*long double*/
+typedef long double ldouble_t;
+METAC_EXPORT_TYPE(ldouble_t);
+
+/* pointers */
+typedef void* voidptr_t;
+typedef void** voidptrptr_t;
+typedef char* charptr_t;
+/* skip other types */
+
+METAC_EXPORT_TYPE(voidptr_t);
+METAC_EXPORT_TYPE(voidptrptr_t);
+METAC_EXPORT_TYPE(charptr_t);
+
+/* enums */
+typedef enum _enum_{
+	_eZero = 0,
+	_eOne,
+	_eTen = 10,
+	_eEleven,
+	_eTwelve,
+}enum_t;
+
+typedef enum{
+	aeZero = 0,
+	aeOne = 1,
+	aeTen = 10,
+	aeEleven,
+	aeTwelve,
+}anon_enum_t;
+METAC_EXPORT_TYPE(enum_t);
+METAC_EXPORT_TYPE(anon_enum_t);
+
+/* arrays */
+typedef char_t char_array5_t[5];
+METAC_EXPORT_TYPE(char_array5_t);
 
 
 struct char_struct_s {
