0a62e9940a2ef75a81222f2e53295c6a9d1eefea
Alexey Odinokov <alexey.odinokov.82@gmail.com>
2024-07-25 20:14:47 -0500

    checking better option

--- include/metac/backend/va_list_ex.h
+++ include/metac/backend/va_list_ex.h
@@ -5,17 +5,11 @@
 
 #include <stdlib.h> /*alloca*/
 
-// this must be big enough to cover stack for alloca
-#define _va_list_padding (int)0x5555, (int)0x5555, (int)0x5555, (int)0x5555
-static inline void va_list_container_start(void** pp, struct va_list_container * p, ...) {
-    *pp = (void*)&(pp); // store the top stack param addr
-    va_start(p->parameters, p);
-}
 /** @brief wrapper macro to put some things into va_list
     example how to get va_list using all this combination of macroses
     WITH_VA_LIST_CONTAINER(c,
-        VA_LIST_CONTAINER(c, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        vsnprintf(buf, sizeof(buf), "%d %d %d %d %d %d %d %d %d %d", c.parameters));
+        vsnprintf(buf, sizeof(buf), "%d %d %d %d %d %d %d %d %d %d", VA_LIST_FROM_CONTAINER(c, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    );
     it outputs 1 2 3 4 5 6 7 8 9 10
     WARNING: even though it works here - there is no guarantee that it will work everywhere and with everything Try not to use it. Reason:
     A va_list is intrinsically linked to a specific function call's stack frame.
@@ -23,15 +17,28 @@ static inline void va_list_container_start(void** pp, struct va_list_container *
     Accessing it in a different function would involve undefined behavior due to potential stack frame changes.
 */
 #define WITH_VA_LIST_CONTAINER(_name_, _in_...) do { \
-        void* pp = NULL; \
         struct va_list_container _name_; \
-        _in_; \
+        _in_ \
         va_end(_name_.parameters); \
     }while(0)
 
-/** @brief macro to get va_list from container and protect it with alloca */
-#define VA_LIST_CONTAINER(_name_, _args_...) \
-    va_list_container_start(&pp, &_name_, _args_, _va_list_padding); \
-    alloca(((char*)&pp)-((char*)pp))
+// this must be big enough to cover stack for alloca
+#define _va_list_padding (int)0x5555, (int)0x5555, (int)0x5555, (int)0x5555
+static inline void va_list_container_start(void** pp, struct va_list_container * p, ...) {
+    *pp = (void*)&(pp); // store the top stack param addr
+    va_start(p->parameters, p);
+}
+/** @brief macro to get va_list from container and protect it with alloca 
+ * alternativly we could use 2 alloca - first is to get the original stack addr
+ * and second is to cover pp value (which is the top of the used by va_list_container_start stack)
+ * this will be more precise 
+*/
+#define VA_LIST_CONTAINER(_name_, _args_...) ({ \
+        void* pp = NULL; \
+        va_list_container_start(&pp, &_name_, _args_, _va_list_padding); \
+        alloca(((char*)&pp)-((char*)pp)); \
+        &_name_; \
+    })
+#define VA_LIST_FROM_CONTAINER(_name_, _args_...) VA_LIST_CONTAINER(_name_, _args_)->parameters
 
 #endif
--- src/va_list_ex_test.c
+++ src/va_list_ex_test.c
@@ -4,21 +4,8 @@
 
 #include "metac/backend/va_list_ex.h"
 
-
-METAC_START_TEST(va_list_ex_sanity) {
-    // this doesn't work on Linux
-    // /home/runner/work/metac/metac/src/va_list_ex_test.c:16:F:default:va_list_ex_sanity:0: expected 1 2 3 4 5 6 7 8 9 10 got 1 -1 0 4 5 -1267418064 1361077456 1361076992 1353769297 1361077488
-    // Windows
-    // D:/a/metac/metac/src/va_list_ex_test.c:16:F:default:va_list_ex_sanity:0: expected 1 2 3 4 5 6 7 8 9 10 got 1024 1592742005 -826280184 4 5 6 7 8 9 10
-    char buf[1024];
-    char *expected_s = NULL;
-    expected_s = "1 2 3 4 5 6 7 8 9 10";
-    WITH_VA_LIST_CONTAINER(c,
-        VA_LIST_CONTAINER(c, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        vsnprintf(buf, sizeof(buf), "%d %d %d %d %d %d %d %d %d %d", c.parameters));
-    fail_unless(strncmp(expected_s, buf, sizeof(buf)) == 0, "expected %s got %s", expected_s, buf);
-
-    // so, lets be very careful with stack
+METAC_START_TEST(va_list_ex_precheck) {
+    // the original test which worked even without alloca protection
     int i, j;
     int fail_i = -1;
     WITH_VA_LIST_CONTAINER(c,
@@ -32,5 +19,33 @@ METAC_START_TEST(va_list_ex_sanity) {
         }
     );
     fail_unless(fail_i < 0, "fail_i: %d", fail_i);
+
+}END_TEST
+
+METAC_START_TEST(va_list_ex_sanity) {
+    char buf[1024];
+    char *expected_s = NULL;
+
+    expected_s = "1 2 3 4 5 6 7 8 9 10";
+    WITH_VA_LIST_CONTAINER(c,
+        vsnprintf(buf, sizeof(buf), "%d %d %d %d %d %d %d %d %d %d", VA_LIST_FROM_CONTAINER(c, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    );
+    fail_unless(strncmp(expected_s, buf, sizeof(buf)) == 0, "expected %s got %s", expected_s, buf);
+
+    WITH_VA_LIST_CONTAINER(c,
+        vsnprintf(buf, sizeof(buf), "%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld", VA_LIST_FROM_CONTAINER(c, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L));
+    );
+    fail_unless(strncmp(expected_s, buf, sizeof(buf)) == 0, "expected %s got %s", expected_s, buf);
+
+    WITH_VA_LIST_CONTAINER(c,
+        vsnprintf(buf, sizeof(buf), "%s %s %s %s %s %s %s %s %s %s", VA_LIST_FROM_CONTAINER(c, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+    );
+    fail_unless(strncmp(expected_s, buf, sizeof(buf)) == 0, "expected %s got %s", expected_s, buf);
+
+    expected_s = "1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0";
+    WITH_VA_LIST_CONTAINER(c,
+        vsnprintf(buf, sizeof(buf), "%.1Lf %.1Lf %.1Lf %.1Lf %.1Lf %.1Lf %.1Lf %.1Lf %.1Lf %.1Lf", VA_LIST_FROM_CONTAINER(c, 1.0L, 2.0L, 3.0L, 4.0L, 5.0L, 6.0L, 7.0L, 8.0L, 9.0L, 10.0L));
+    );
+    fail_unless(strncmp(expected_s, buf, sizeof(buf)) == 0, "expected %s got %s", expected_s, buf);
 
 }END_TEST
