9a516e6b916180b9740829b7bb5a0a9c8bbbc8f7
Alexey Odinokov <alexey.odinokov.82@gmail.com>
2024-08-15 19:40:47 -0500

    made some TODO: to improve bitfields

--- src/value.c
+++ src/value.c
@@ -122,12 +122,19 @@ static int _entry_bitfield_read(metac_entry_t *p_memb_entry, void * base_addr, v
     metac_offset_t byte_offset;
     metac_offset_t bit_offset;
     metac_offset_t bit_size;
+    uint8_t buf[16];
 
     _check_(metac_entry_member_bitfield_offsets(p_memb_entry, &byte_offset, &bit_offset, &bit_size) != 0, -(EINVAL));
 
      /* TODO: Caution: this has been tested only with little-endian machines*/
     if (IS_LITTLE_ENDIAN) {
         base_addr += byte_offset;
+        // read only number of bytes that we really need
+        // TODO: make it without extra copy if possible
+        // TODO: need to make it for both endians + for write
+        assert(bit_size != 0);
+        memcpy(buf, base_addr, (bit_offset + bit_size-1)/8 +1);
+        base_addr = buf;
     } else {
         assert(metac_entry_parent_count(p_memb_entry) == 1);
         metac_entry_t * p_memb_parent = metac_entry_parent_entry(p_memb_entry, 0);
