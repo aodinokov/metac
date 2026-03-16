e5b2a9d3b6489838cfd7f57840cb884778de9de1
Alexey Odinokov <alexey.odinokov.82@gmail.com>
2026-03-15 23:27:04 -0500

    John Doe is a name placeholder, not Joe

--- src/serialization/cjson/cjson_test.c
+++ src/serialization/cjson/cjson_test.c
@@ -940,7 +940,7 @@ typedef struct {
 	} marital_status;
 }test11_t;
 
-test11_t *p_t11_src = (test11_t[]){{.firstname = "Joe", .lastname = "Doe", .age=43, .marital_status=msDivorsed}}, *p_t11_dst = NULL;
+test11_t *p_t11_src = (test11_t[]){{.firstname = "John", .lastname = "Doe", .age=43, .marital_status=msDivorsed}}, *p_t11_dst = NULL;
 
 METAC_GSYM_LINK(p_t11_src);
 METAC_GSYM_LINK(p_t11_dst);
@@ -974,7 +974,7 @@ METAC_START_TEST(test11_satnity) {
             fail_unless(p_t11_dst->firstname != NULL && p_t11_dst->firstname != p_t11_src->firstname && strcmp(p_t11_dst->firstname, p_t11_src->firstname) == 0, "exected p_t11_dst->firstname %s to be equial to p_t11_src->firstname %s", p_t11_dst->firstname, p_t11_src->firstname);
             fail_unless(p_t11_dst->lastname != NULL && p_t11_dst->lastname != p_t11_src->lastname && strcmp(p_t11_dst->lastname, p_t11_src->lastname) == 0, "exected p_t11_dst->lastname %s to be equial to p_t11_src->lastname %s", p_t11_dst->lastname, p_t11_src->lastname);
         ),
-        "{\"firstname\":\"Joe\",\"lastname\":\"Doe\",\"age\":43,\"marital_status\":\"msDivorsed\"}"
+        "{\"firstname\":\"John\",\"lastname\":\"Doe\",\"age\":43,\"marital_status\":\"msDivorsed\"}"
     );
 
     metac_value_delete(p_val_dst);
